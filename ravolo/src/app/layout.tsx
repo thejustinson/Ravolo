71924bed48ac649f1c2558fe15a9ccdf2848a160
Nwachukwu Justin <63079391+thejustinson@users.noreply.github.com>
2024-05-31 12:20:37 +0100

    init

---
+++ ravolo/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Ravolo",
+  description: "Ravolo leverages blockchain technology to create a secure, transparent, and innovative experience for event organizers and attendees.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
